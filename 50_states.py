c16ba5ab523592ed28e675ff26c6f3a1a7c3007a
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2021-06-25 21:28:07 +0200

    Added game play and functionality to save missed states

--- 50_states.py
+++ 50_states.py
@@ -6,21 +6,45 @@ screen.title("U.S. states")
 
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
+pen_draw = turtle.Turtle()
+pen_draw.hideturtle()
+pen_draw.penup()
 
 states = pd.read_csv("50_states.csv")
 
-# while True:
-answer = screen.textinput("Enter the state","What's another name?")
-if answer in states.state:
-    turtle.goto(states[states.state == "answer"].x,states[states.state == "answer"].y)
-    print(states[states.state == "answer"].x)
-    turtle.write(states[states.state == "answer"].state)
 
-print(answer)
+def lower(s):
+    return s.lower()
+
+states_list = states.state.to_list()
+guessed_states = []
 
 
-turtle.mainloop()
+
+
+while len(guessed_states) < 50:
+    answer = screen.textinput((f"{len(guessed_states)}/50"),"Enter the state").title()
+    if answer == "Exit":
+        break
+
+    if answer in states.state.to_list():
+        guessed_states.append(answer)
+        x_coor = states[states["state"] == answer].x
+        y_coor = states[states["state"] == answer].y
+
+        pen_draw.goto(x_coor.to_list()[0],y_coor.to_list()[0])
+        pen_draw.write(states[states.state == answer].state.to_list()[0]) 
+        # can use the original answer or the below
+        # pen_draw.write(states.item())
+
+
+missed_states = [x for x in states_list if x not in guessed_states]
+missed_states = pd.DataFrame(missed_states)
+missed_states.to_csv("missed_states.csv")
+
+
+
+
 
 
